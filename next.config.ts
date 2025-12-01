e66f06cb0c6ea7549ba5483552c0091981240033
HasanMadeIt <cloudslippershabbo@gmail.com>
2025-12-01 14:05:50 +0000

    nuked

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  // Optional: customize other stuff if needed
-};
-
-export default nextConfig;
